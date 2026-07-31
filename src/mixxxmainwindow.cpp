f41c970afb5e488b3bd100661b13b8fca6e8a925
agentr303 <agentrichie@popstar.com>
2026-07-31 17:02:19 +1000

    Update mixxxmainwindow.cpp
    pass MidiClockOutputManager into DlgPreferences constructor in mixxxmainwindow

--- src/mixxxmainwindow.cpp
+++ src/mixxxmainwindow.cpp
@@ -312,6 +312,7 @@ void MixxxMainWindow::initialize() {
             m_pSkinLoader,
             m_pCoreServices->getSoundManager(),
             m_pCoreServices->getControllerManager(),
+            m_pCoreServices->getMidiClockOutputManager(),
             m_pCoreServices->getVinylControlManager(),
             m_pCoreServices->getEffectsManager(),
             m_pCoreServices->getSettingsManager(),
