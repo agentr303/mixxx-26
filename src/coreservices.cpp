69693b746798c6c50b6791ce0be5549653b6adf8
agentr303 <agentrichie@popstar.com>
2026-08-12 10:31:35 +1000

    Update coreservices.cpp

--- src/coreservices.cpp
+++ src/coreservices.cpp
@@ -288,9 +288,6 @@ void CoreServices::initialize(QApplication* pApp) {
             m_pEffectsManager.get(),
             pChannelHandleFactory,
             true);
-    m_pMidiClockOutputManager = std::make_shared<MidiClockOutputManager>(m_pControllerManager.get());
-    m_pEngine->setMidiClockGenerator(m_pMidiClockOutputManager->generator());
-    m_pControllerManager->setMidiClockOutputManager(m_pMidiClockOutputManager.get());
 #ifdef __RUBBERBAND__
     RubberBandWorkerPool::createInstance(pConfig);
 #endif
